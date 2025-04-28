e4a3452bbc169670898ceed6d8f8d8a78c670f3a
Andrew Moore <moore6aw@mail.uc.edu>
2025-04-28 16:03:17 -0400

    Strong password checker

--- Joey_FinalProject/Default.aspx.cs
+++ Joey_FinalProject/Default.aspx.cs
@@ -24,10 +24,10 @@ namespace Joey_FinalProject
 
         protected void Abtn_Click(object sender, EventArgs e)
         {
-            lblheader.Text = " ";
-            lbldescription.Text = " ";
-            lbltstcase.Text = " ";
-            lbloutput.Text = " ";
+            lblheader.Text = "420. Strong Password Checker ";
+            lbldescription.Text = "You enter a password that has to be at least 6 characters and at most 20 charaacters, It has to contain at least one lowercase letter and it does not contain three repeating characters in a row for the password to be strong. This code will output a number if it is correct.  ";
+            lbltstcase.Text = "jerseymike, 123478, andrew";
+            lbloutput.Text = "2, 2, 2";
         }
 
         protected void ADbtn_Click(object sender, EventArgs e)
